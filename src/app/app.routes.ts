9b8021c1ba8c7c9c28db0a231bfc1757ae1d7abc
nictru <nictru32@gmail.com>
2025-05-10 01:21:44 +0200

    Add basic app structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+import { StartComponent } from './main/start/start.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'app',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: StartComponent,
+      },
+    ],
   },
 ];
