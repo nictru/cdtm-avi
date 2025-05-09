c69a6094b9b1beea8ce96cfbfef2baed90801f01
nictru <nictru32@gmail.com>
2025-05-10 01:59:44 +0200

    Implement emergency disclaimer

--- src/app/main/new-appointment/new-appointment.component.ts
+++ src/app/main/new-appointment/new-appointment.component.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { AppointmentTypeComponent } from './appointment-type/appointment-type.component';
-
+import { EmergencyComponent } from './emergency/emergency.component';
 @Component({
   selector: 'app-new-appointment',
-  imports: [AppointmentTypeComponent],
+  imports: [AppointmentTypeComponent, EmergencyComponent],
   templateUrl: './new-appointment.component.html',
   styleUrl: './new-appointment.component.css',
 })
